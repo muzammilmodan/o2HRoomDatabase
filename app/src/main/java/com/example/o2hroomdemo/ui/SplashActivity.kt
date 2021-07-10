e0b47a0020490a73599e3762bcfb19f9d43d95c5
mujjuRaja <muzammil.intelivita@gmail.com>
2021-07-10 14:51:41 +0530

    10/07/2021

--- app/src/main/java/com/example/o2hroomdemo/ui/SplashActivity.kt
+++ app/src/main/java/com/example/o2hroomdemo/ui/SplashActivity.kt
@@ -1,13 +1,18 @@
 package com.example.o2hroomdemo.ui
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.multidex.MultiDex
 import com.example.o2hroomdemo.R
 import com.example.o2hroomdemo.ui.login.LoginActivity
 import com.example.o2hroomdemo.ui.main.MainActivity
+import com.example.o2hroomdemo.utils.SessionManager
 import java.util.*
 
 class SplashActivity : AppCompatActivity() {
@@ -24,7 +29,10 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         mContext = SplashActivity@ this
-        splashTimeOut()
+
+        if (checkPermission()) {
+            splashTimeOut()
+        }
     }
 
     private fun splashTimeOut() {
@@ -33,7 +41,7 @@ class SplashActivity : AppCompatActivity() {
             timer.schedule(object : TimerTask() {
 
                 override fun run() {
-                    if(com.example.o2hroomdemo.utils.SessionManager.getIsUserLoggedin(mContext)) {
+                    if(SessionManager.getIsUserLoggedin(mContext)) {
                         val intent = Intent(this@SplashActivity, MainActivity::class.java)
                         intent.flags =
                             Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
@@ -52,4 +60,51 @@ class SplashActivity : AppCompatActivity() {
         }
 
     }
+
+    private val MY_PERMISSIONS_REQUEST = 152
+
+    private fun checkPermission(): Boolean {
+        try {
+
+            val ReadPermission =
+                ContextCompat.checkSelfPermission(mContext, Manifest.permission.READ_EXTERNAL_STORAGE)
+            val WritePermission =
+                ContextCompat.checkSelfPermission(mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+            val listPermissionsNeeded = ArrayList<String>()
+
+            if (ReadPermission != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+            if (WritePermission != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+            if (!listPermissionsNeeded.isEmpty()) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    listPermissionsNeeded.toTypedArray(), MY_PERMISSIONS_REQUEST
+                )
+                return false
+            }
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return true
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == MY_PERMISSIONS_REQUEST) {
+            splashTimeOut()
+        }
+    }
+
 }
